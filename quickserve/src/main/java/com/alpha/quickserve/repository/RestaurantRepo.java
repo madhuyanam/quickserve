4a9ca8de19b2d5d62715feae748a9b0882db33b1
KatrothRajesh <rajeshkatroth333@gmail.com>
2026-02-14 09:58:32 +0530

    commit

--- quickserve/src/main/java/com/alpha/quickserve/repository/RestaurantRepo.java
+++ quickserve/src/main/java/com/alpha/quickserve/repository/RestaurantRepo.java
@@ -3,9 +3,11 @@ package com.alpha.quickserve.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.alpha.quickserve.entity.Restaurant;
+
 import jakarta.annotation.Resource;
 
 @Repository
-public interface RestaurantRepo extends JpaRepository<Resource, Integer>{
+public interface RestaurantRepo extends JpaRepository<Restaurant, Integer>{
 
 }
