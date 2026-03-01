74a1295674523d3ce67ced0f902aba581156348b
ChaithanyaC4 <21d41a04c4@gmail.com>
2026-03-01 19:33:29 +0530

    okk

--- src/main/java/com/alpha/quickserve/QuickserveApplication.java
+++ src/main/java/com/alpha/quickserve/QuickserveApplication.java
@@ -3,8 +3,11 @@ package com.alpha.quickserve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 @SpringBootApplication
 public class QuickserveApplication {
 
@@ -18,19 +21,19 @@ public class QuickserveApplication {
         return new RestTemplate();
     }
 
-//	@Bean
-//	  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-//
-//	    RedisTemplate<String, String> template = new RedisTemplate<>();
-//	    template.setConnectionFactory(connectionFactory);
-//
-//	    template.setKeySerializer(new StringRedisSerializer());
-//	    template.setValueSerializer(new StringRedisSerializer());
-//
-//	    template.afterPropertiesSet();
-//	    return template;
-//	  }
-//	
+	@Bean
+	  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+	    RedisTemplate<String, String> template = new RedisTemplate<>();
+	    template.setConnectionFactory(connectionFactory);
+
+	    template.setKeySerializer(new StringRedisSerializer());
+	    template.setValueSerializer(new StringRedisSerializer());
+
+	    template.afterPropertiesSet();
+	    return template;
+	  }
+	
 	
 	
 }
